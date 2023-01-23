0f1a181d37e57336c3a2931bfc1c67cb1b8b3251
uberkie <44478849+uberkie@users.noreply.github.com>
2023-01-23 05:32:29 +0200

    Update guiapp.py
    remove extra code

--- guiapp.py
+++ guiapp.py
@@ -3,10 +3,19 @@ from tkinter import messagebox
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+from tkinter import *
 
 # Create the main window
 root = tk.Tk()
-root.title("Excel Editor")
+root.title("NW Storage")
+#sets the position of the main window 
+
+root.attributes("-alpha", 0.7)
+root.focus_set()
+root.geometry('500x350+1100+50')
+root.update_idletasks()
+
+
 
 # Read the xlsx file into a pandas dataframe
 df = pd.read_excel("storage.xlsx")
@@ -14,8 +23,10 @@ df = pd.read_excel("storage.xlsx")
 # Create a label and entry widget for the search query
 query_label = tk.Label(root, text="Search Query:")
 query_label.grid(row=0, column=0)
+query_label.place(height=40, width=100)
 query_entry = tk.Entry(root)
 query_entry.grid(row=0, column=1)
+query_entry.place(height=30, width=100,x=100,y=2)
 
 def search():
     # Get the search query
@@ -37,7 +48,7 @@ def search():
     # Insert the filtered data into the text widget
             for index, row in filtered_df.iterrows():
                 result_text.insert(tk.END, f"Storage: {row['Storage']}\n")
-                result_text.insert(tk.END, f"Weptype: {row['Type']}\n")
+                result_text.insert(tk.END, f"Type: {row['Type']}\n")
                 result_text.insert(tk.END, f"Tier: {row['Tier']}\n")
                 result_text.insert(tk.END, f"Class: {row['Class']}\n")
                 result_text.insert(tk.END, f"Name: {row['Name']}\n")
@@ -52,61 +63,17 @@ def search():
                 result_text.insert(tk.END, f"BoP: {row['BoP']}\n")
                 result_text.insert(tk.END, "\n")
                 
-                        # Create a text widget to display the data
+# Create a text widget to display the data
 
                 
 search_button = tk.Button(root, text="Search", command=search)
-search_button.grid(row=0, column=2)
+search_button.grid(row=0, column=1)
+root.columnconfigure(2, weight=1)
+search_button.place(height=30, width=100,x=210,y=2)
 
 # Create a text widget to display the search results
 result_text = tk.Text(root)
-result_text.grid(row=1, column=0, columnspan=3)
-                
-                
-## new window
-def open_window_2():
-    window_2 = tk.Toplevel(root)
-    window_2.title("Title 2")
-    # Create a label to display 
-
-# Create Entry widgets for row, column, and new value
-row_label = tk.Label(root, text="Enter Row Index:")
-row_label.grid(row=2, column=0)
-row_entry = tk.Entry(root)
-row_entry.grid(row=2, column=1)
-
-column_label = tk.Label(root, text="Enter Column:")
-column_label.grid(row=3, column=0)
-column_entry = tk.Entry(root)
-column_entry.grid(row=3, column=1)
-
-value_label = tk.Label(root, text="Enter New Value:")
-value_label.grid(row=4, column=0)
-value_entry = tk.Entry(root)
-value_entry.grid(row=4, column=1)
-def update_cell():
-    # Get the row and column indexes from the user input
-    row = int(row_entry.get())
-    column = column_entry.get()
-
-    # Get the new value from the user input
-    new_value = value_entry.get()
-
-    try:
-        # Update the cell in the DataFrame
-        df.at[row, column] = new_value
-
-        # Write the changes to the original xlsx file
-        df.to_excel('file.xlsx', index=False)
-        messagebox.showinfo("Success", "Data has been updated Successfully.")
-    except:
-        messagebox.showerror("Error", "Data could not be updated.")
-# Create update button
-update_button = tk.Button(root, text="Update", command=update_cell)
-update_button.grid(row=3, column=2)
-
-
-
-
+result_text.grid(row=1, column=0, columnspan=1)
+result_text.place(width=350, height=200,x=10,y=100)
 
 root.mainloop()
