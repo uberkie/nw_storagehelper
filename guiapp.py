4bf0bf89960c54e45c75ed33b542d7d2386100c3
uberkie <44478849+uberkie@users.noreply.github.com>
2023-02-04 22:32:03 +0200

    updated to sqlite3.db

--- guiapp.py
+++ guiapp.py
@@ -1,79 +1,141 @@
 import tkinter as tk
-from tkinter import messagebox
-import pandas as pd
-from pandas import ExcelWriter
-from pandas import ExcelFile
-from tkinter import *
+import tkinter.ttk as ttk
+import sqlite3
+import tkinter.simpledialog as simpledialog
 
-# Create the main window
 root = tk.Tk()
-root.title("NW Storage")
-#sets the position of the main window 
 
-root.attributes("-alpha", 0.7)
-root.focus_set()
-root.geometry('500x350+1100+50')
-root.update_idletasks()
+# Connect to the database
+conn = sqlite3.connect("nw_storagehelper/newworld.db")
+cursor = conn.cursor()
 
+# Fetch the data
+cursor.execute("SELECT * FROM storage")
+data = cursor.fetchall()
 
+# Set up the treeview widget
+tree = ttk.Treeview(root,
+                    columns=["id", "Storage", "Type", "Tier", "Class", "Name", "Gearscore", "CON", "STR", "DEX", "INT",
+                             "FOC", "Perks", "BoE", "BoP"], show="headings")
 
-# Read the xlsx file into a pandas dataframe
-df = pd.read_excel("storage.xlsx")
+tree.heading("id", text="id", anchor="center")
+tree.heading("Storage", text="Storage", anchor="center")
+tree.heading("Type", text="Type")
+tree.heading("Tier", text="Tier")
+tree.heading("Class", text="Class")
+tree.heading("Name", text="Name")
+tree.heading("Gearscore", text="Gearscore")
+tree.heading("CON", text="CON")
+tree.heading("STR", text="STR")
+tree.heading("DEX", text="DEX")
+tree.heading("INT", text="INT")
+tree.heading("FOC", text="FOC")
+tree.heading("Perks", text="Perks")
+tree.heading("BoE", text="BoE")
+tree.heading("BoP", text="BoP")
+tree.column("id", width=100, stretch=False)
+tree.column("Storage", width=100, stretch=False)
+tree.column("Type", width=100, stretch=False, anchor="center")
+tree.column("Tier", width=100, stretch=False, anchor="center")
+tree.column("Class", width=100, stretch=False, anchor="center")
+tree.column("Name", width=100, stretch=False, anchor="center")
+tree.column("Gearscore", width=100, stretch=False, anchor="center")
+tree.column("CON", width=100, stretch=False, anchor="center")
+tree.column("STR", width=100, stretch=False, anchor="center")
+tree.column("DEX", width=100, stretch=False, anchor="center")
+tree.column("INT", width=100, stretch=False, anchor="center")
+tree.column("FOC", width=100, stretch=False)
+tree.column("Perks", width=100, stretch=False, anchor="center")
+tree.column("BoE", width=100, stretch=False, anchor="center")
+tree.column("BoP", width=100, stretch=False, anchor="center")
 
-# Create a label and entry widget for the search query
-query_label = tk.Label(root, text="Search Query:")
-query_label.grid(row=0, column=0)
-query_label.place(height=40, width=100)
-query_entry = tk.Entry(root)
-query_entry.grid(row=0, column=1)
-query_entry.place(height=30, width=100,x=100,y=2)
+# Insert the data into the treeview
+for index, row in enumerate(data):
+    tree.insert("", index, text=str(index), values=row)
+
+# Add a search entry widget
+label_entry = tk.Label(root, text="Search for something")
+label_entry.pack()
+search_var = tk.StringVar()
+entry = tk.Entry(root, textvariable=search_var)
+entry.pack()
+
 
 def search():
-    # Get the search query
-    query = query_entry.get()
-    if query == [int]:
-        columns_to_search = ["Name", "CON", "STR", "DEX", "INT", "FOC", "Perks", "BoE", "BoP"]
-        filtered_df = df.copy()
-        for col in columns_to_search:
-            filtered_df = filtered_df[filtered_df[col] == int(query)]
-    else:
-        # Filter the dataframe based on the query
-        colm = ["Name", "CON", "STR", "DEX", "INT", "FOC", "BoE", "BoP"]
-        for colms in colm:
-            filtered_df = df[(df['Perks'].str.contains(query, case=False)) | (df['Type'].str.contains(query, case=False) | (df['Name'].str.contains(query, case=False)))]
-            # Clear the text widget
-            result_text.delete("1.0", tk.END)
+    # Get the search term and split it by the delimiter of "+"
+    search_terms = search_var.get().lower().split("+")
+
+    # Clear the treeview
+    for item in tree.get_children():
+        tree.delete(item)
+
+    # Search for items that match all of the search terms
+    for index, row in enumerate(data):
+        if all([any([search_term in str(x).lower() for x in row]) for search_term in search_terms]):
+            tree.insert("", index, text=str(index), values=row)
 
 
-    # Insert the filtered data into the text widget
-            for index, row in filtered_df.iterrows():
-                result_text.insert(tk.END, f"Storage: {row['Storage']}\n")
-                result_text.insert(tk.END, f"Type: {row['Type']}\n")
-                result_text.insert(tk.END, f"Tier: {row['Tier']}\n")
-                result_text.insert(tk.END, f"Class: {row['Class']}\n")
-                result_text.insert(tk.END, f"Name: {row['Name']}\n")
-                result_text.insert(tk.END, f"Gearscore: {row['Gearscore']}\n")
-                result_text.insert(tk.END, f"CON: {row['CON']}\n")
-                result_text.insert(tk.END, f"STR: {row['STR']}\n")
-                result_text.insert(tk.END, f"DEX: {row['DEX']}\n")
-                result_text.insert(tk.END, f"INT: {row['INT']}\n")
-                result_text.insert(tk.END, f"FOC: {row['FOC']}\n")
-                result_text.insert(tk.END, f"Perks: {row['Perks']}\n")
-                result_text.insert(tk.END, f"BoE: {row['BoE']}\n")
-                result_text.insert(tk.END, f"BoP: {row['BoP']}\n")
-                result_text.insert(tk.END, "\n")
-                
-# Create a text widget to display the data
+label2_entry = tk.Label(root, text="Search for 2 things")
+label2_entry.pack()
 
-                
-search_button = tk.Button(root, text="Search", command=search)
-search_button.grid(row=0, column=1)
-root.columnconfigure(2, weight=1)
-search_button.place(height=30, width=100,x=210,y=2)
 
-# Create a text widget to display the search results
-result_text = tk.Text(root)
-result_text.grid(row=1, column=0, columnspan=1)
-result_text.place(width=350, height=200,x=10,y=100)
+# Create a sorting function
+def sort_tree(tree, col, reverse):
+    l = [(tree.set(k, col), k) for k in tree.get_children('')]
+    l.sort(reverse=reverse)
+
+    for index, (val, k) in enumerate(l):
+        tree.move(k, '', index)
+
+    tree.heading(col, command=lambda: sort_tree(tree, col, not reverse))
+
+
+# Sort the Treeview in ascending order
+sort_tree(tree, "Storage", False)
+sort_tree(tree, "Type", False)
+sort_tree(tree, "Tier", False)
+sort_tree(tree, "Class", False)
+sort_tree(tree, "Name", False)
+sort_tree(tree, "Gearscore", False)
+sort_tree(tree, "CON", False)
+sort_tree(tree, "STR", False)
+sort_tree(tree, "DEX", False)
+sort_tree(tree, "INT", False)
+sort_tree(tree, "FOC", False)
+sort_tree(tree, "Perks", False)
+sort_tree(tree, "BoE", False)
+sort_tree(tree, "BoP", False)
+
+# Call the search function when the user types in the search bar
+entry.bind("<Key>", lambda event: search())
+
+
+def update_database(item, column, new_value):
+    # Update the database
+    cursor.execute("UPDATE storage SET {}=? WHERE ID=?".format(column), (new_value, item))
+    conn.commit()
+    cursor.execute("SELECT * FROM storage")
+
+
+def delete_data():
+    # Get the selected row
+    selected_item = tree.selection()[0]
+    uid = tree.item(selected_item)['values'][0]
+
+    # Delete the selected row from the database
+    del_query = "DELETE FROM  storage WHERE id=?"
+    sel_data = (uid,)
+    conn.execute(del_query, sel_data)
+    conn.commit()
+
+    # Delete the selected row from the treeview
+    tree.delete(selected_item)
+
+
+# Add a button to delete the selected row
+delete_button = tk.Button(root, text="Delete", command=delete_data)
+delete_button.pack()
+
+tree.pack()
 
 root.mainloop()
